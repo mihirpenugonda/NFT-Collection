f7a6770e0926a64a51c4247c4460092cf569e537
Mihr Penugonda <mihirpenugonda@gmail.com>
2022-02-20 14:13:03 +0000

    commit images and small changes

--- nft_hardhat/constants.js
+++ nft_hardhat/constants.js
@@ -1,4 +1,4 @@
 WHITELIST_CONTRACT_ADDRESS = "0x9Aec3a591370F123C4560c5576af1D1124bEa399";
-METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+METADATA_URL = "https://nft-collection-seven-tau.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
